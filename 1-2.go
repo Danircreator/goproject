cd599ffba2dacdbab949b82a4412fe9fbee9db39
Danir_doma <danirkashapov@gmail.com>
2023-02-26 21:07:51 +0500

    Second tasks

---
+++ 1-2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int
+    
+    fmt.Scanln(&a)
+    
+    fmt.Println("The next number for the number", a, "is", a+1,("."))
+    fmt.Println("The previous number for the number", a, "is", a-1)
+    
+}
