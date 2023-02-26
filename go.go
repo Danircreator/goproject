f461b68c59fd8a5de8f2acfcfde86c63a6aaeb9b
Danir_doma <danirkashapov@gmail.com>
2023-02-26 14:07:25 +0500

    First

--- go.go
+++ go.go
@@ -4,13 +4,5 @@ import "fmt"
 
 
 func main() {
-  my_print(6,4,1,4,2,123,1)
-}
-
-func my_print(nums ...int){
-  
-  for _, nums := range nums{
-  fmt.Println(nums)
-  }
-
+  fmt.Prinln("git test")
 }
